bb7064242ad2c39d648efa0fc0ff099d37c5b389
gvidas <danieliusgvidas@gmail.com>
2023-12-09 15:09:55 +0000

    basic flock rules

--- Assets/FlockingController.cs
+++ Assets/FlockingController.cs
@@ -9,23 +9,33 @@ using Random = UnityEngine.Random;
 
 public class FlockingController : MonoBehaviour{
 
+    [Header("Fish")]
     [SerializeField] private GameObject FishesGO;
     [SerializeField] private GameObject FishPrefab;
-
-    private List<Fish> fishes = new List<Fish>();
-
     [SerializeField] private int fishAmt;
     [SerializeField] private float fishSpeed;
     [SerializeField] private float visionDist;
-    [SerializeField] private float repulsionVisionDist;
+    
+    [Header("Barrel")]
     [SerializeField] private float barrelSize;
     [SerializeField] private float barrelEdgeSize;
     [SerializeField] private float edgeForce;
-    [SerializeField][Range(0f,.99f)] private float alignmentStrength;
+    
+    [Header("Repulsion")]
+    [SerializeField] private float repulsionVisionDist;
     [SerializeField] private float repulsionStrength;
     [SerializeField] private float maxRepulsion;
+    
+    [Header("Cohesion")]
+    [SerializeField][Range(0f,.99f)] private float cohesionStrength;
+
+    [Header("Alignment")]
+    [SerializeField][Range(0f,.99f)] private float alignmentStrength;
 
 
+
+    private List<Fish> fishes = new List<Fish>();
+    
     private struct Fish{
         public Vector2 pos;
         public Vector2 vel;
@@ -45,19 +55,14 @@ public class FlockingController : MonoBehaviour{
             GameObject newFishGO = Instantiate(FishPrefab, pos, Quaternion.identity, FishesGO.transform);
         }
     }
-
+    //update fish game objects to line up with fishes array
     private void UpdateFish(){
         for (int i = 0; i < fishAmt; i++) {
             Transform child = FishesGO.transform.GetChild(i);
             Fish fish = fishes[i];
-            try {
-                SetFishGOPos(fish, child);
-            }
-            catch (Exception e) {
-                print($"error {e}, {i} {fish.pos} {fish.vel}");
-                throw;
-            }
-
+            
+            SetFishGOPos(fish, child);
+            
             Vector3 vel = new Vector3(fish.vel.x, 0, fish.vel.y);
             child.transform.LookAt(child.transform.position + vel);
         }
@@ -66,23 +71,26 @@ public class FlockingController : MonoBehaviour{
     private void Update(){
         for (int i = 0; i < fishAmt; i++) {
             Fish fish = fishes[i];
-            fish.pos += fish.vel * Time.deltaTime;
+            fish.pos += fish.vel / 100;
             float distToCenter = Vector2.Distance(fish.pos, Vector2.zero);
 
             Vector2 totalVel = Vector2.zero;
             Vector2 totalPos = Vector2.zero;
             Vector2 totalRepulsion = Vector2.zero;
-
+            float fishInRange = 0;
+            float fishInRangeRep = 0;
             for (int j = 0; j < fishAmt; j++) {
                 if (i != j) {
                     float dist = Vector2.Distance(fish.pos, fishes[j].pos);
                     if (dist < visionDist) {
+                        fishInRange++;
                         // alignment
                         totalVel += fishes[j].vel;
                         // cohesion
                         totalPos += fishes[j].pos;
                         // repulsion
                         if (dist < repulsionVisionDist) {
+                            fishInRangeRep++;
                             Vector2 vecToFish = (fishes[j].pos - fish.pos).normalized;
                             Vector2 fishRep = vecToFish * (1 / Vector2.Distance(fish.pos, fishes[j].pos));
                             totalRepulsion += fishRep;
@@ -93,25 +101,41 @@ public class FlockingController : MonoBehaviour{
                     }   
                 }
             }
-            
-
-            
-            Vector2 avgVel = totalVel / fishAmt; //alignment
-            Vector2 avgPos = totalPos / fishAmt; //cohesion
-            Vector2 avgRepulsion = totalRepulsion / fishAmt; //repulsion
 
 
-            //alignment
-            Vector2 newVel = Vector2.Lerp(fish.vel, avgVel, alignmentStrength);
-            fish.vel = newVel;
-            
-
-            Vector2 repulsion = avgRepulsion * repulsionStrength;
-            if (repulsion.magnitude > maxRepulsion) {
-                repulsion = repulsion.normalized * maxRepulsion;
+            if (fishInRange > 0) {
+                Vector2 avgVel = totalVel / fishAmt; //alignment
+                Vector2 avgPos = totalPos / fishInRange; //cohesion
+                
+                //alignment
+                Vector2 newVel = Vector2.Lerp(fish.vel, avgVel, alignmentStrength);
+                fish.vel = newVel;
+                
+                //cohesion
+                Vector2 cohesion = Vector2.Lerp(fish.pos, avgPos, cohesionStrength);
+                fish.vel -= (fish.pos - cohesion);
             }
 
-            fish.vel -= repulsion;
+            if (fishInRangeRep > 0) {
+                Vector2 avgRepulsion = totalRepulsion / fishAmt; //repulsion
+                
+                //repulsion
+                Vector2 repulsion = avgRepulsion * repulsionStrength;
+                if (repulsion.magnitude > maxRepulsion) {
+                    repulsion = repulsion.normalized * maxRepulsion;
+                }
+                fish.vel -= repulsion;
+            }
+
+
+
+            
+
+            
+            
+            
+            
+            
             
             //reflect if outside of barrel
             if (distToCenter > barrelSize) {
@@ -129,7 +153,7 @@ public class FlockingController : MonoBehaviour{
 
             
             
-            
+            //lerp to normalise
             float speedMag = fish.vel.magnitude;
             fish.vel = fish.vel * fishSpeed / speedMag;
 
